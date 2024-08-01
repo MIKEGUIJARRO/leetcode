d6984e40f7cdd023a85cb74a94368c698260473d
Mike Guijarro <mikeguijarrodev@gmail.com>
2024-07-31 23:42:24 -0600

    6 - Add Binary Search

---
+++ search/Binary Search.py
@@ -0,0 +1,25 @@
+from typing import List
+
+# returns the integer of the searched value
+def binary_search(arr: List, search_val: int) -> int:
+    low = 0
+    high = len(arr) - 1
+
+    while  low <= high:
+        mid = int((high + low) / 2)
+
+        guess = arr[mid]
+
+        if guess == search_val:
+            return mid
+        
+        if guess < search_val:
+            low = guess + 1
+        else:
+            high = guess - 1
+
+    return None
+
+arr = [1, 2, 4, 5, 6, 8, 12, 24, 89]
+
+res = binary_search(arr, 24)
